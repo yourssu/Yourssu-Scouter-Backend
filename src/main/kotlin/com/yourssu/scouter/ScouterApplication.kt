faa466c9e3da9138c1bc3037d7d5d121398305dd
kwonyj1022 <kwonyj1022@naver.com>
2025-01-23 03:52:48 +0900

    chore: 스프링부트 프로젝트 생성
    - `start.spring.io`에서 생성
    - 추가한 Dependencies
      - Spring Web
      - Spring Data JPA
      - H2 Database
      - MySQL Driver
      - Validation

---
+++ src/main/kotlin/com/yourssu/scouter/ScouterApplication.kt
@@ -0,0 +1,11 @@
+package com.yourssu.scouter
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ScouterApplication
+
+fun main(args: Array<String>) {
+	runApplication<ScouterApplication>(*args)
+}
