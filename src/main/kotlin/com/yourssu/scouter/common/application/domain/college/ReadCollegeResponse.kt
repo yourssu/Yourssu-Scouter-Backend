18f556c4c211c7ad1136b11724e212211a12201f
kwonyj1022 <kwonyj1022@naver.com>
2025-01-23 23:26:08 +0900

    fix: 단과대 목록 조회 응답 dto 필드명을 api 명세에 맞게 수정

--- src/main/kotlin/com/yourssu/scouter/common/application/domain/college/ReadCollegeResponse.kt
+++ src/main/kotlin/com/yourssu/scouter/common/application/domain/college/ReadCollegeResponse.kt
@@ -3,14 +3,14 @@ package com.yourssu.scouter.common.application.domain.college
 import com.yourssu.scouter.common.business.domain.college.CollegeDto
 
 data class ReadCollegeResponse(
-    val id: Long,
-    val name: String,
+    val collegeId: Long,
+    val collegetName: String,
 ) {
 
     companion object {
         fun from(collegeDto: CollegeDto) = ReadCollegeResponse(
-            id = collegeDto.id,
-            name = collegeDto.name,
+            collegeId = collegeDto.id,
+            collegetName = collegeDto.name,
         )
     }
 }
