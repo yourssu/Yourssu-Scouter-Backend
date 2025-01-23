42951b83d19709d8e51953843ecb22c887b7d82e
kwonyj1022 <kwonyj1022@naver.com>
2025-01-23 23:18:57 +0900

    feat: 단과대 id로 학과 목록을 조회하는 service 메서드 추가

---
+++ src/main/kotlin/com/yourssu/scouter/common/business/domain/department/DepartmentService.kt
@@ -0,0 +1,17 @@
+package com.yourssu.scouter.common.business.domain.department
+
+import com.yourssu.scouter.common.implement.domain.department.Department
+import com.yourssu.scouter.common.implement.domain.department.DepartmentReader
+import org.springframework.stereotype.Service
+
+@Service
+class DepartmentService(
+    private val departmentReader: DepartmentReader,
+) {
+
+    fun readAllByCollegeId(collegeId: Long): ReadDepartmentsResult {
+        val departments: List<Department> = departmentReader.readAllByCollegeId(collegeId)
+
+        return ReadDepartmentsResult.from(departments)
+    }
+}
---
+++ src/main/kotlin/com/yourssu/scouter/common/business/domain/department/ReadDepartmentsResult.kt
@@ -0,0 +1,16 @@
+package com.yourssu.scouter.common.business.domain.department
+
+import com.yourssu.scouter.common.implement.domain.department.Department
+
+data class ReadDepartmentsResult(
+    val departmentDtos: List<DepartmentDto>,
+) {
+
+    companion object {
+        fun from(departments: List<Department>): ReadDepartmentsResult {
+            return ReadDepartmentsResult(
+                departmentDtos = departments.map { DepartmentDto.from(it) },
+            )
+        }
+    }
+}
