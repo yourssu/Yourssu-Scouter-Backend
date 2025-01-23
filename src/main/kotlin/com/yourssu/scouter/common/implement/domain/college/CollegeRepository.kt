d1fc0d9d0df636480d188c7ee33c0509528e78d0
kwonyj1022 <kwonyj1022@naver.com>
2025-01-24 01:59:56 +0900

    remove: 기존에 구현된 단과대 초기 데이터 저장 로직 제거

--- src/main/kotlin/com/yourssu/scouter/common/implement/domain/college/CollegeRepository.kt
+++ src/main/kotlin/com/yourssu/scouter/common/implement/domain/college/CollegeRepository.kt
@@ -4,8 +4,6 @@ import org.springframework.stereotype.Repository
 
 @Repository
 interface CollegeRepository {
-
-    fun saveAll(colleges: List<College>)
     fun count(): Long
     fun findAll(): List<College>
 }
--- src/main/kotlin/com/yourssu/scouter/common/storage/domain/college/CollegeRepositoryImpl.kt
+++ src/main/kotlin/com/yourssu/scouter/common/storage/domain/college/CollegeRepositoryImpl.kt
@@ -9,10 +9,6 @@ class CollegeRepositoryImpl(
     private val jpaCollegeRepository: JpaCollegeRepository,
 ) : CollegeRepository {
 
-    override fun saveAll(colleges: List<College>) {
-        jpaCollegeRepository.saveAll(colleges.map { CollegeEntity.from(it) })
-    }
-
     override fun count(): Long {
         return jpaCollegeRepository.count()
     }
