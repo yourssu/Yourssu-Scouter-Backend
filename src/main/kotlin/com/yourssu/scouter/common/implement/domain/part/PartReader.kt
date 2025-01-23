9fdba20c37c528877d9adca43c3a45fa98f96152
kwonyj1022 <kwonyj1022@naver.com>
2025-01-24 02:48:04 +0900

    feat: 파트 목록을 조회하는 reader 메서드 추가

---
+++ src/main/kotlin/com/yourssu/scouter/common/implement/domain/part/PartReader.kt
@@ -0,0 +1,15 @@
+package com.yourssu.scouter.common.implement.domain.part
+
+import org.springframework.stereotype.Component
+import org.springframework.transaction.annotation.Transactional
+
+@Component
+@Transactional(readOnly = true)
+class PartReader(
+    private val partRepository: PartRepository,
+) {
+
+    fun readAll(): List<Part> {
+        return partRepository.findAll()
+    }
+}
