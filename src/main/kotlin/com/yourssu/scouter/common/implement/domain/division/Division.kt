8d463615483354dbeb3f33722e4e04b937927a29
kwonyj1022 <kwonyj1022@naver.com>
2025-01-24 02:14:09 +0900

    feat: 구분 domain 추가

---
+++ src/main/kotlin/com/yourssu/scouter/common/implement/domain/division/Division.kt
@@ -0,0 +1,24 @@
+package com.yourssu.scouter.common.implement.domain.division
+
+class Division(
+    val id: Long? = null,
+    val name: String,
+) {
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as Division
+
+        return id == other.id
+    }
+
+    override fun hashCode(): Int {
+        return id?.hashCode() ?: 0
+    }
+
+    override fun toString(): String {
+        return "Division(id=$id, name='$name')"
+    }
+}
