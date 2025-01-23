c39ff1ccbac4cc10ae7613d104995721b2e1804d
kwonyj1022 <kwonyj1022@naver.com>
2025-01-24 02:20:43 +0900

    feat: 구분 entity 추가

---
+++ src/main/kotlin/com/yourssu/scouter/common/storage/domain/division/DivisionEntity.kt
@@ -0,0 +1,38 @@
+package com.yourssu.scouter.common.storage.domain.division
+
+import jakarta.persistence.Column
+import jakarta.persistence.Entity
+import jakarta.persistence.GeneratedValue
+import jakarta.persistence.GenerationType
+import jakarta.persistence.Id
+import jakarta.persistence.Table
+
+@Entity
+@Table(name = "division")
+class DivisionEntity(
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    val id: Long? = null,
+
+    @Column(nullable = false, unique = true)
+    val name: String,
+) {
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as DivisionEntity
+
+        return id == other.id
+    }
+
+    override fun hashCode(): Int {
+        return id?.hashCode() ?: 0
+    }
+
+    override fun toString(): String {
+        return "DivisionEntity(id=$id, name='$name')"
+    }
+}
