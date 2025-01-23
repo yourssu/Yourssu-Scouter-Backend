6cbd31a8738cdb7a850519bceb778f564d38a971
kwonyj1022 <kwonyj1022@naver.com>
2025-01-24 02:30:47 +0900

    feat: 구분 목록을 조회하는 service 메서드 추가

---
+++ src/main/kotlin/com/yourssu/scouter/common/business/domain/division/ReadDivisionsResult.kt
@@ -0,0 +1,16 @@
+package com.yourssu.scouter.common.business.domain.division
+
+import com.yourssu.scouter.common.implement.domain.division.Division
+
+data class ReadDivisionsResult(
+    val divisionDtos: List<DivisionDto>,
+) {
+
+    companion object {
+        fun from(divisions: List<Division>): ReadDivisionsResult {
+            return ReadDivisionsResult(
+                divisionDtos = divisions.map { DivisionDto.from(it) },
+            )
+        }
+    }
+}
