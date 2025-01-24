ecb614ddfebb31589e3a0aafb652881178674e2d
kwonyj1022 <kwonyj1022@naver.com>
2025-01-25 04:09:44 +0900

    feat: 멤버를 저장하는 service 메서드 추가

--- src/main/kotlin/com/yourssu/scouter/hrms/business/domain/member/MemberService.kt
+++ src/main/kotlin/com/yourssu/scouter/hrms/business/domain/member/MemberService.kt
@@ -1,13 +1,28 @@
 package com.yourssu.scouter.hrms.business.domain.member
 
+import com.yourssu.scouter.common.implement.domain.department.DepartmentReader
+import com.yourssu.scouter.common.implement.domain.part.PartReader
 import com.yourssu.scouter.hrms.implement.domain.member.Member
 import com.yourssu.scouter.hrms.implement.domain.member.MemberReader
+import com.yourssu.scouter.hrms.implement.domain.member.MemberWriter
 import org.springframework.stereotype.Service
 
 @Service
 class MemberService(
+    private val memberWriter: MemberWriter,
     private val memberReader: MemberReader,
+    private val departmentReader: DepartmentReader,
+    private val partReader: PartReader,
 ) {
+
+    fun create(command: CreateMemberCommand) {
+        val department = departmentReader.readById(command.departmentId)
+        val part = partReader.readById(command.partId)
+
+        val member: Member = command.toDomain(department, part)
+
+        memberWriter.write(member)
+    }
 
     fun readById(memberId: Long): MemberDto {
         val member: Member = memberReader.readById(memberId)
