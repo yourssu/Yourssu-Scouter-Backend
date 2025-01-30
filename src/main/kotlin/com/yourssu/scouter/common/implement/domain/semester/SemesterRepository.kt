f22544e43873a44ba8b52fa48c44151751fb61e5
kwonyj1022 <kwonyj1022@naver.com>
2025-01-30 23:14:36 +0900

    feat: 학기 저장, 삭제 repository 메서드 추가

--- src/main/kotlin/com/yourssu/scouter/common/implement/domain/semester/SemesterRepository.kt
+++ src/main/kotlin/com/yourssu/scouter/common/implement/domain/semester/SemesterRepository.kt
@@ -2,6 +2,8 @@ package com.yourssu.scouter.common.implement.domain.semester
 
 interface SemesterRepository {
 
+    fun save(semester: Semester): Semester
     fun findById(semesterId: Long): Semester?
     fun findAll(): List<Semester>
+    fun deleteById(semesterId: Long)
 }
--- src/main/kotlin/com/yourssu/scouter/common/storage/domain/semester/SemesterRepositoryImpl.kt
+++ src/main/kotlin/com/yourssu/scouter/common/storage/domain/semester/SemesterRepositoryImpl.kt
@@ -10,11 +10,19 @@ class SemesterRepositoryImpl(
     private val jpaSemesterRepository: JpaSemesterRepository,
 ) : SemesterRepository {
 
+    override fun save(semester: Semester): Semester {
+        return jpaSemesterRepository.save(SemesterEntity.from(semester)).toDomain()
+    }
+
     override fun findById(semesterId: Long): Semester? {
         return jpaSemesterRepository.findByIdOrNull(semesterId)?.toDomain()
     }
 
     override fun findAll(): List<Semester> {
         return jpaSemesterRepository.findAll().map { it.toDomain() }
+    }
+
+    override fun deleteById(semesterId: Long) {
+        jpaSemesterRepository.deleteById(semesterId)
     }
 }
