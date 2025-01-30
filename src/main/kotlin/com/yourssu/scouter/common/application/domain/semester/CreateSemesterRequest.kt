5f3af7d3882f9d831ab46fa98fbb4cb6873f5585
kwonyj1022 <kwonyj1022@naver.com>
2025-01-30 23:20:52 +0900

    feat: 학기 저장, 삭제 api 추가

---
+++ src/main/kotlin/com/yourssu/scouter/common/application/domain/semester/CreateSemesterRequest.kt
@@ -0,0 +1,15 @@
+package com.yourssu.scouter.common.application.domain.semester
+
+import jakarta.validation.constraints.NotNull
+import jakarta.validation.constraints.Positive
+
+data class CreateSemesterRequest(
+
+    @NotNull(message = "연도를 입력하지 않았습니다.")
+    @Positive(message = "연도는 양수여야 합니다.")
+    val year: Int,
+
+    @NotNull(message = "학기를 입력하지 않았습니다.")
+    @Positive(message = "학기는 양수여야 합니다.")
+    val term: Int,
+)
--- src/main/kotlin/com/yourssu/scouter/common/application/domain/semester/SemesterController.kt
+++ src/main/kotlin/com/yourssu/scouter/common/application/domain/semester/SemesterController.kt
@@ -2,8 +2,14 @@ package com.yourssu.scouter.common.application.domain.semester
 
 import com.yourssu.scouter.common.business.domain.semester.SemesterDto
 import com.yourssu.scouter.common.business.domain.semester.SemesterService
+import jakarta.validation.Valid
+import java.net.URI
 import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.DeleteMapping
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
@@ -11,11 +17,29 @@ class SemesterController(
     private val semesterService: SemesterService,
 ) {
 
+    @PostMapping("/semesters")
+    fun create(
+        @RequestBody @Valid request: CreateSemesterRequest,
+    ): ResponseEntity<Unit> {
+        val semesterId: Long = semesterService.create(request.year, request.term)
+
+        return ResponseEntity.created(URI.create("/semesters/$semesterId")).build()
+    }
+
     @GetMapping("/semesters")
     fun readAll(): ResponseEntity<List<ReadSemesterResponse>> {
         val semesterDtos: List<SemesterDto> = semesterService.readAll()
         val response: List<ReadSemesterResponse> = semesterDtos.map { ReadSemesterResponse.from(it) }
 
         return ResponseEntity.ok(response)
+    }
+
+    @DeleteMapping("/semesters/{semesterId}")
+    fun deleteById(
+        @PathVariable semesterId: Long,
+    ): ResponseEntity<Unit> {
+        semesterService.deleteById(semesterId)
+
+        return ResponseEntity.noContent().build()
     }
 }
