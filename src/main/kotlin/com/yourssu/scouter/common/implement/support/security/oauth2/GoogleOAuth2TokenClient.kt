8b927912c9d28b19d1c6b01d879fad8821cb2388
kwonyj1022 <kwonyj1022@naver.com>
2025-02-01 00:46:08 +0900

    feat: 구글 액세스 토큰과 리프레시 토큰을 발급받는 Feign Client 추가

---
+++ src/main/kotlin/com/yourssu/scouter/common/implement/support/security/oauth2/GoogleOAuth2TokenClient.kt
@@ -0,0 +1,28 @@
+package com.yourssu.scouter.common.implement.support.security.oauth2
+
+import com.fasterxml.jackson.databind.PropertyNamingStrategies
+import com.fasterxml.jackson.databind.annotation.JsonNaming
+import org.springframework.cloud.openfeign.FeignClient
+import org.springframework.http.MediaType
+import org.springframework.util.MultiValueMap
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
+
+@FeignClient(name = "googleOAuth2TokenClient", url = "https://oauth2.googleapis.com")
+interface GoogleOAuth2TokenClient {
+
+    @PostMapping(
+        "/token",
+        consumes = [MediaType.APPLICATION_FORM_URLENCODED_VALUE]
+    )
+    fun fetchToken(@RequestBody params: MultiValueMap<String, String>): GoogleTokenResponse
+}
+
+@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy::class)
+data class GoogleTokenResponse(
+    val accessToken: String,
+    val expiresIn: Long,
+    val refreshToken: String,
+    val scope: String,
+    val tokenType: String,
+)
