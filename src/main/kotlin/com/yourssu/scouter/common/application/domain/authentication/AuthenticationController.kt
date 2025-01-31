e24134697cc87c3a081f1a2dc44fc4d22ef0f9d7
kwonyj1022 <kwonyj1022@naver.com>
2025-02-01 02:18:44 +0900

    feat: 소셜 로그인 api 추가

---
+++ src/main/kotlin/com/yourssu/scouter/common/application/domain/authentication/AuthenticationController.kt
@@ -0,0 +1,29 @@
+package com.yourssu.scouter.common.application.domain.authentication
+
+import com.yourssu.scouter.common.business.domain.authentication.AuthenticationService
+import com.yourssu.scouter.common.business.domain.authentication.LoginResult
+import com.yourssu.scouter.common.implement.domain.authentication.OAuth2Type
+import jakarta.validation.Valid
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+class AuthenticationController(
+    private val authenticationService: AuthenticationService,
+) {
+
+    @PostMapping("oauth2/login/{oauth2Type}")
+    fun login(
+        @PathVariable oauth2Type: OAuth2Type,
+        @RequestBody @Valid request: OAuth2LoginRequest,
+    ): ResponseEntity<LoginResponse> {
+
+        val loginResult: LoginResult = authenticationService.login(oauth2Type, request.authorizationCode)
+        val response: LoginResponse = LoginResponse.from(loginResult)
+
+        return ResponseEntity.ok(response)
+    }
+}
