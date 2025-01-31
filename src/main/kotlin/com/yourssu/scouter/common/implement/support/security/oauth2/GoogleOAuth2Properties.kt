efa5371e18dbc7778f3bf26d1ff671f31324ae87
kwonyj1022 <kwonyj1022@naver.com>
2025-01-31 17:24:20 +0900

    feat: 설정 파일에서 구글 OAuth2 설정값을 가져오는 기능 추가

---
+++ src/main/kotlin/com/yourssu/scouter/common/implement/support/security/oauth2/GoogleOAuth2Properties.kt
@@ -0,0 +1,11 @@
+package com.yourssu.scouter.common.implement.support.security.oauth2
+
+import org.springframework.boot.context.properties.ConfigurationProperties
+
+@ConfigurationProperties(prefix = "oauth2.google")
+data class GoogleOAuth2Properties(
+    val clientId: String,
+    val clientSecret: String,
+    val relativeRedirectUri: String,
+    val scope: List<String>
+)
