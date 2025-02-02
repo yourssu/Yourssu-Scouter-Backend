78aa794ad281400117d588c8497bd85f1c11924a
kwonyj1022 <kwonyj1022@naver.com>
2025-02-02 17:41:46 +0900

    feat: 멤버 기본 정보, 상태, 역할 수정 시의 service 메서드 추가

--- src/main/kotlin/com/yourssu/scouter/hrms/business/domain/member/MemberService.kt
+++ src/main/kotlin/com/yourssu/scouter/hrms/business/domain/member/MemberService.kt
@@ -4,6 +4,8 @@ import com.yourssu.scouter.common.implement.domain.department.Department
 import com.yourssu.scouter.common.implement.domain.department.DepartmentReader
 import com.yourssu.scouter.common.implement.domain.part.Part
 import com.yourssu.scouter.common.implement.domain.part.PartReader
+import com.yourssu.scouter.common.implement.domain.semester.Semester
+import com.yourssu.scouter.hrms.business.support.exception.IllegalMemberUpdateException
 import com.yourssu.scouter.hrms.business.support.utils.MemberRoleConverter
 import com.yourssu.scouter.hrms.business.support.utils.MemberStateConverter
 import com.yourssu.scouter.hrms.implement.domain.member.ActiveMember
@@ -15,6 +17,7 @@ import com.yourssu.scouter.hrms.implement.domain.member.MemberRole
 import com.yourssu.scouter.hrms.implement.domain.member.MemberState
 import com.yourssu.scouter.hrms.implement.domain.member.MemberWriter
 import com.yourssu.scouter.hrms.implement.domain.member.WithdrawnMember
+import java.time.LocalDate
 import org.springframework.stereotype.Service
 
 @Service
@@ -82,6 +85,127 @@ class MemberService(
         val members: List<WithdrawnMember> = memberReader.searchAllWithdrawnByNameOrNickname(query)
 
         return members.map { WithdrawnMemberDto.from(it) }
+    }
+
+    private fun updateMemberInfo(targetMemberId: Long, command: UpdateMemberInfoCommand) {
+        countNotNullFields(command)
+        val target: Member = memberReader.readById(targetMemberId)
+        if (command.role != null) {
+            updateMemberRole(target, command.role)
+        }
+        if (command.state != null) {
+            updateMemberState(target, command.state)
+        }
+
+        Member(
+            id = target.id,
+            name = command.name ?: target.name,
+            email = command.email ?: target.email,
+            phoneNumber = command.phoneNumber ?: target.phoneNumber,
+            birthDate = command.birthDate ?: target.birthDate,
+            department = command.departmentId?.let { departmentReader.readById(it) } ?: target.department,
+            studentId = command.studentId ?: target.studentId,
+            parts = command.partIds?.let { partReader.readAllByIds(it) } ?: target.parts,
+            role = target.role,
+            nicknameEnglish = command.nicknameEnglish ?: target.nicknameEnglish,
+            nicknameKorean = command.nicknameKorean ?: target.nicknameKorean,
+            state = target.state,
+            joinDate = command.joinDate ?: target.joinDate,
+            note = command.note ?: target.note,
+        )
+    }
+
+    private fun countNotNullFields(command: UpdateMemberInfoCommand): Int {
+        return listOf(
+            command.name,
+            command.email,
+            command.phoneNumber,
+            command.birthDate,
+            command.departmentId,
+            command.studentId,
+            command.partIds,
+            command.role,
+            command.nicknameEnglish,
+            command.nicknameKorean,
+            command.state,
+            command.joinDate,
+            command.note,
+        ).count { it != null }
+    }
+
+    private fun updateMemberRole(target: Member, newRole: MemberRole) {
+        var newNote = ""
+        if (newRole in listOf(MemberRole.LEAD, MemberRole.VICE_LEAD)) {
+            val (currentYear, currentTerm) = Semester.of(LocalDate.now()).run { year to term.intValue }
+            val partName: String = target.parts.first().name // TODO : 1멤버 2파트인 경우 어떻게 처리할지 물어보고 수정하기
+            val newRoleName: String = MemberRoleConverter.convertToString(newRole)
+
+            newNote = "${currentYear}년 ${currentTerm}학기 $partName 파트 $newRoleName 역임\n"
+        }
+
+        val updateMember = Member(
+            id = target.id,
+            name = target.name,
+            email = target.email,
+            phoneNumber = target.phoneNumber,
+            birthDate = target.birthDate,
+            department = target.department,
+            studentId = target.studentId,
+            parts = target.parts,
+            role = newRole,
+            nicknameEnglish = target.nicknameEnglish,
+            nicknameKorean = target.nicknameKorean,
+            state = target.state,
+            joinDate = target.joinDate,
+            note = "${newNote}${target.note}",
+        )
+
+        memberWriter.update(updateMember)
+    }
+
+    private fun updateMemberState(target: Member, newState: MemberState) {
+        val updateMember = Member(
+            id = target.id,
+            name = target.name,
+            email = target.email,
+            phoneNumber = target.phoneNumber,
+            birthDate = target.birthDate,
+            department = target.department,
+            studentId = target.studentId,
+            parts = target.parts,
+            role = target.role,
+            nicknameEnglish = target.nicknameEnglish,
+            nicknameKorean = target.nicknameKorean,
+            state = newState,
+            joinDate = target.joinDate,
+            note = target.note,
+        )
+
+        when (newState) {
+            MemberState.ACTIVE -> {
+                memberWriter.writeMemberWithActiveStatus(
+                    member = updateMember,
+                )
+            }
+
+            MemberState.INACTIVE -> {
+                memberWriter.writeMemberWithInactiveState(
+                    member = updateMember,
+                    currentDate = LocalDate.now(),
+                )
+            }
+
+            MemberState.GRADUATED -> {
+                memberWriter.writeMemberWithGraduatedState(
+                    member = updateMember,
+                    currentDate = LocalDate.now(),
+                )
+            }
+
+            MemberState.WITHDRAWN -> {
+                memberWriter.writeMemberWithWithdrawnState(updateMember)
+            }
+        }
     }
 
     fun readAllRoles(): List<String> {
