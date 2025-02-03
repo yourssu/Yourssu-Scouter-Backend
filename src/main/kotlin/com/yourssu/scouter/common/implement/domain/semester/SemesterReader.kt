5138c1b7a19400a468718c91c0e89a52a79035ea
kwonyj1022 <kwonyj1022@naver.com>
2025-02-04 04:38:01 +0900

    feat: 학기 문자열에 해당하는 학기를 조회하는 reader 메서드 추가

--- src/main/kotlin/com/yourssu/scouter/common/implement/domain/semester/SemesterReader.kt
+++ src/main/kotlin/com/yourssu/scouter/common/implement/domain/semester/SemesterReader.kt
@@ -1,6 +1,7 @@
 package com.yourssu.scouter.common.implement.domain.semester
 
 import com.yourssu.scouter.common.implement.support.exception.SemesterNotFoundException
+import java.time.Year
 import org.springframework.stereotype.Component
 import org.springframework.transaction.annotation.Transactional
 
@@ -19,4 +20,16 @@ class SemesterReader(
     }
 
     fun readAll(): List<Semester> = semesterRepository.findAll()
+
+    fun readByString(applicationSemesterString: String): Semester {
+        val yearString: String = applicationSemesterString.substringBefore("-")
+        val yearValue = yearString.toInt() % 100 + 2000
+        val termString: String = applicationSemesterString.substringAfter("-")
+
+        val year: Year = Year.of(yearValue)
+        val term: Term = Term.of(termString.toInt())
+        val toFind = Semester(year = year, term = term)
+
+        return semesterRepository.find(toFind) ?: throw SemesterNotFoundException("지정한 학기를 찾을 수 없습니다.")
+    }
 }
