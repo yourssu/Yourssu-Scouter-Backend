8bd19abc0ae4d2a79a3b30857a06c5f5315b5a5c
kwonyj1022 <kwonyj1022@naver.com>
2025-02-05 06:17:34 +0900

    feat: 인증을 위한 Interceptor 추가

---
+++ src/main/kotlin/com/yourssu/scouter/common/application/support/authentication/LoginInterceptor.kt
@@ -0,0 +1,29 @@
+package com.yourssu.scouter.common.application.support.authentication
+
+import com.yourssu.scouter.common.application.support.exception.LoginRequiredException
+import com.yourssu.scouter.common.implement.domain.authentication.TokenProcessor
+import com.yourssu.scouter.common.implement.domain.authentication.TokenType
+import com.yourssu.scouter.common.implement.support.exception.InvalidTokenException
+import jakarta.servlet.http.HttpServletRequest
+import jakarta.servlet.http.HttpServletResponse
+import org.springframework.http.HttpHeaders
+import org.springframework.stereotype.Component
+import org.springframework.web.servlet.HandlerInterceptor
+
+@Component
+class LoginInterceptor(
+    private val tokenProcessor: TokenProcessor,
+) : HandlerInterceptor {
+
+    override fun preHandle(request: HttpServletRequest, response: HttpServletResponse, handler: Any): Boolean {
+        val accessToken: String? = request.getHeader(HttpHeaders.AUTHORIZATION)
+        if (accessToken.isNullOrEmpty()) {
+            throw LoginRequiredException("로그인이 필요한 기능입니다.")
+        }
+
+        tokenProcessor.decode(TokenType.ACCESS, accessToken)
+            ?: throw InvalidTokenException("유효한 토큰이 아닙니다.")
+
+        return true
+    }
+}
