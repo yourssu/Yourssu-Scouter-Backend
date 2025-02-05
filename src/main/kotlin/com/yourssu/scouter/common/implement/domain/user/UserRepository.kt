810dce0a964a3eea1d12f93acece5618b3b7a210
kwonyj1022 <kwonyj1022@naver.com>
2025-02-05 11:39:23 +0900

    feat: 유저 존재 여부를 확인하는 repository, reader 메서드 추가

--- src/main/kotlin/com/yourssu/scouter/common/implement/domain/user/UserRepository.kt
+++ src/main/kotlin/com/yourssu/scouter/common/implement/domain/user/UserRepository.kt
@@ -3,6 +3,7 @@ package com.yourssu.scouter.common.implement.domain.user
 interface UserRepository {
 
     fun save(user: User): User
+    fun existsById(userId: Long): Boolean
     fun findById(userId: Long): User?
     fun findByOAuthId(oauthId: String): User?
 }
