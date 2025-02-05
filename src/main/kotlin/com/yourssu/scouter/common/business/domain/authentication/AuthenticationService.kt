8801d4b2948b08c4867c8f11c05e3d2a8703a917
kwonyj1022 <kwonyj1022@naver.com>
2025-02-05 11:41:06 +0900

    feat: 로그아웃 service 메서드 추가

--- src/main/kotlin/com/yourssu/scouter/common/business/domain/authentication/AuthenticationService.kt
+++ src/main/kotlin/com/yourssu/scouter/common/business/domain/authentication/AuthenticationService.kt
@@ -1,5 +1,8 @@
 package com.yourssu.scouter.common.business.domain.authentication
 
+import com.yourssu.scouter.common.business.support.exception.NoSuchUserException
+import com.yourssu.scouter.common.implement.domain.authentication.BlacklistTokenReader
+import com.yourssu.scouter.common.implement.domain.authentication.BlacklistTokenWriter
 import com.yourssu.scouter.common.implement.domain.authentication.OAuth2TokenInfo
 import com.yourssu.scouter.common.implement.domain.authentication.OAuth2Type
 import com.yourssu.scouter.common.implement.domain.authentication.OAuth2User
@@ -9,6 +12,8 @@ import com.yourssu.scouter.common.implement.domain.authentication.TokenType
 import com.yourssu.scouter.common.implement.domain.user.User
 import com.yourssu.scouter.common.implement.domain.user.UserReader
 import com.yourssu.scouter.common.implement.domain.user.UserWriter
+import com.yourssu.scouter.common.implement.support.exception.InvalidTokenException
+import io.jsonwebtoken.Claims
 import java.time.LocalDateTime
 import org.springframework.stereotype.Service
 
@@ -18,6 +23,8 @@ class AuthenticationService(
     private val userReader: UserReader,
     private val userWriter: UserWriter,
     private val tokenProcessor: TokenProcessor,
+    private val blacklistTokenWriter: BlacklistTokenWriter,
+    private val blacklistTokenReader: BlacklistTokenReader,
 ) {
 
     fun login(oauth2Type: OAuth2Type, oauth2AuthorizationCode: String): LoginResult {
@@ -70,6 +77,32 @@ class AuthenticationService(
         user.updateToken(newTokenInfo)
 
         return userWriter.write(user)
+    }
+
+    fun logout(accessToken: String, refreshToken: String) {
+        val userId = getValidUserId(TokenType.ACCESS, accessToken)
+
+        blacklistTokenWriter.write(userId, accessToken, refreshToken)
+    }
+
+    private fun getValidUserId(tokenType: TokenType, token: String): Long {
+        val privateClaims: PrivateClaims = decode(tokenType, token)
+        val userId: Long = privateClaims.userId
+        if (!userReader.existsById(userId)) {
+            throw NoSuchUserException("존재하지 않는 유저의 토큰입니다.")
+        }
+        if (blacklistTokenReader.isBlacklisted(userId, token)) {
+            throw InvalidTokenException("로그아웃되었습니다.")
+        }
+
+        return userId
+    }
+
+    private fun decode(tokenType: TokenType, accessToken: String): PrivateClaims {
+        val claims: Claims = tokenProcessor.decode(tokenType, accessToken)
+            ?: throw InvalidTokenException("유효한 토큰이 아닙니다.")
+
+        return PrivateClaims.from(claims)
     }
 }
 
