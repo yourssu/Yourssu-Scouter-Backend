430a9d7a405eeaba50afd8a513f04d8fe1598722
kwonyj1022 <kwonyj1022@naver.com>
2025-02-05 12:43:15 +0900

    refactor: interceptor와 인증 serivce의 중복 코드 제거

--- src/main/kotlin/com/yourssu/scouter/common/application/support/authentication/AuthenticationInterceptor.kt
+++ src/main/kotlin/com/yourssu/scouter/common/application/support/authentication/AuthenticationInterceptor.kt
@@ -1,12 +1,7 @@
 package com.yourssu.scouter.common.application.support.authentication
 
-import com.yourssu.scouter.common.business.support.exception.NoSuchUserException
-import com.yourssu.scouter.common.implement.domain.authentication.BlacklistTokenReader
-import com.yourssu.scouter.common.implement.domain.authentication.PrivateClaims
-import com.yourssu.scouter.common.implement.domain.authentication.TokenProcessor
+import com.yourssu.scouter.common.business.domain.authentication.AuthenticationService
 import com.yourssu.scouter.common.implement.domain.authentication.TokenType
-import com.yourssu.scouter.common.implement.domain.user.UserReader
-import com.yourssu.scouter.common.implement.support.exception.InvalidTokenException
 import jakarta.servlet.http.HttpServletRequest
 import jakarta.servlet.http.HttpServletResponse
 import org.springframework.http.HttpHeaders
@@ -15,9 +10,7 @@ import org.springframework.web.servlet.HandlerInterceptor
 
 @Component
 class AuthenticationInterceptor(
-    private val tokenProcessor: TokenProcessor,
-    private val userReader: UserReader,
-    private val blacklistTokenReader: BlacklistTokenReader,
+    private val authenticationService: AuthenticationService,
 ) : HandlerInterceptor {
 
     override fun preHandle(request: HttpServletRequest, response: HttpServletResponse, handler: Any): Boolean {
@@ -26,17 +19,7 @@ class AuthenticationInterceptor(
             return true
         }
 
-        val claims = (tokenProcessor.decode(TokenType.ACCESS, accessToken)
-            ?: throw InvalidTokenException("유효한 토큰이 아닙니다."))
-        val privateClaims = PrivateClaims.from(claims)
-
-        val userId = privateClaims.userId
-        if (!userReader.existsById(userId)) {
-            throw NoSuchUserException("존재하지 않는 사용자의 토큰입니다.")
-        }
-        if (blacklistTokenReader.isBlacklisted(userId, accessToken)) {
-            throw InvalidTokenException("로그아웃되었습니다.")
-        }
+        authenticationService.getValidPrivateClaims(TokenType.ACCESS, accessToken)
 
         return true
     }
