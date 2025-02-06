645ea8705f280b4b24597f933121abb6864a4150
kwonyj1022 <kwonyj1022@naver.com>
2025-02-06 20:51:24 +0900

    feat: 인증 관련 Interceptor와 Argumentresolver 설정 추가

---
+++ src/main/kotlin/com/yourssu/scouter/common/application/support/configuration/WebConfiguration.kt
@@ -0,0 +1,24 @@
+package com.yourssu.scouter.common.application.support.configuration
+
+import com.yourssu.scouter.common.application.support.authentication.AuthUserInfoArgumentResolver
+import com.yourssu.scouter.common.application.support.authentication.LoginInterceptor
+import org.springframework.context.annotation.Configuration
+import org.springframework.web.method.support.HandlerMethodArgumentResolver
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
+
+@Configuration
+class WebConfiguration(
+    private val loginInterceptor: LoginInterceptor,
+    val authUserInfoArgumentResolver: AuthUserInfoArgumentResolver
+) : WebMvcConfigurer {
+
+    override fun addInterceptors(registry: InterceptorRegistry) {
+        registry.addInterceptor(loginInterceptor)
+            .addPathPatterns("/**")
+    }
+
+    override fun addArgumentResolvers(resolvers: MutableList<HandlerMethodArgumentResolver>) {
+        resolvers.add(authUserInfoArgumentResolver)
+    }
+}
