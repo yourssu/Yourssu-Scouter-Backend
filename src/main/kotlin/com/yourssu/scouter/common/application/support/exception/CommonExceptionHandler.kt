3ce914e8a8774a5e24d4567efbbc1680cd4e4e05
kwonyj1022 <kwonyj1022@naver.com>
2025-02-14 23:34:45 +0900

    feat: 전역 예외 처리 핸들러 추가

--- src/main/kotlin/com/yourssu/scouter/common/application/support/exception/CommonExceptionHandler.kt
+++ src/main/kotlin/com/yourssu/scouter/common/application/support/exception/CommonExceptionHandler.kt
@@ -2,19 +2,42 @@ package com.yourssu.scouter.common.application.support.exception
 
 import com.yourssu.scouter.common.implement.support.exception.CustomException
 import java.util.stream.Collectors
+import org.springframework.http.HttpHeaders
 import org.springframework.http.HttpStatus
+import org.springframework.http.HttpStatusCode
 import org.springframework.http.ResponseEntity
 import org.springframework.http.converter.HttpMessageNotReadableException
 import org.springframework.validation.FieldError
 import org.springframework.web.bind.MethodArgumentNotValidException
 import org.springframework.web.bind.annotation.ExceptionHandler
 import org.springframework.web.bind.annotation.RestControllerAdvice
+import org.springframework.web.context.request.WebRequest
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler
 
 @RestControllerAdvice
-class CommonExceptionHandler {
+class CommonExceptionHandler: ResponseEntityExceptionHandler() {
 
     companion object {
         private const val METHOD_ARGUMENT_NOT_VALID_EXCEPTION_ERROR_CODE = "Request-Validation-Fail"
+        private const val LOG_MESSAGE_FORMAT = "%s : %s"
+    }
+
+    override fun handleExceptionInternal(
+        ex: Exception,
+        body: Any?,
+        headers: HttpHeaders,
+        statusCode: HttpStatusCode,
+        request: WebRequest
+    ): ResponseEntity<Any>? {
+        logger.error(String.format(LOG_MESSAGE_FORMAT, ex.javaClass.simpleName, ex.message), ex)
+
+        val response = ExceptionResponse(
+            status = HttpStatus.valueOf(statusCode.value()),
+            errorCode = "Internal-Server-Error",
+            message = ex.message ?: "Internal server error"
+        )
+
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(response)
     }
 
     @ExceptionHandler(MethodArgumentNotValidException::class)
