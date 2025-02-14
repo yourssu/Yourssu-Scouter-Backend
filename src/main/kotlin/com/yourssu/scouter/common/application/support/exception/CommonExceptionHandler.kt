342209ff6e7ec4c09feed8258fdc1f57884059d9
권예진 <96688810+kwonyj1022@users.noreply.github.com>
2025-02-15 00:16:21 +0900

    fix: #68 회원가입, 재로그인 시 발생하는 오류 해결 (#71)
    * feat: 구글 사용자 정보 가져올 때 null값 허용
    
    * fix: 로그아웃 후 재로그인 시 예외 발생하는 문제 해결
    
    * feat: 전역 예외 처리 핸들러 추가
    
    * feat: 예외 처리 핸들러에 로깅 추가

--- src/main/kotlin/com/yourssu/scouter/common/application/support/exception/CommonExceptionHandler.kt
+++ src/main/kotlin/com/yourssu/scouter/common/application/support/exception/CommonExceptionHandler.kt
@@ -2,53 +2,90 @@ package com.yourssu.scouter.common.application.support.exception
 
 import com.yourssu.scouter.common.implement.support.exception.CustomException
 import java.util.stream.Collectors
+import org.springframework.http.HttpHeaders
 import org.springframework.http.HttpStatus
+import org.springframework.http.HttpStatusCode
 import org.springframework.http.ResponseEntity
 import org.springframework.http.converter.HttpMessageNotReadableException
 import org.springframework.validation.FieldError
 import org.springframework.web.bind.MethodArgumentNotValidException
 import org.springframework.web.bind.annotation.ExceptionHandler
 import org.springframework.web.bind.annotation.RestControllerAdvice
+import org.springframework.web.context.request.WebRequest
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler
 
 @RestControllerAdvice
-class CommonExceptionHandler {
+class CommonExceptionHandler: ResponseEntityExceptionHandler() {
 
     companion object {
         private const val METHOD_ARGUMENT_NOT_VALID_EXCEPTION_ERROR_CODE = "Request-Validation-Fail"
+        private const val LOG_MESSAGE_FORMAT = "%s : %s"
     }
 
-    @ExceptionHandler(MethodArgumentNotValidException::class)
-    fun handleMethodArgumentNotValidException(
-        e: MethodArgumentNotValidException,
-    ): ResponseEntity<ExceptionResponse> {
-        val message = e.fieldErrors
+    override fun handleExceptionInternal(
+        ex: Exception,
+        body: Any?,
+        headers: HttpHeaders,
+        statusCode: HttpStatusCode,
+        request: WebRequest
+    ): ResponseEntity<Any>? {
+        logger.error(String.format(LOG_MESSAGE_FORMAT, ex.javaClass.simpleName, ex.message), ex)
+
+        val response = ExceptionResponse(
+            status = HttpStatus.valueOf(statusCode.value()),
+            errorCode = "Internal-Server-Error",
+            message = ex.message ?: "Internal server error"
+        )
+
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(response)
+    }
+
+    override fun handleMethodArgumentNotValid(
+        ex: MethodArgumentNotValidException,
+        headers: HttpHeaders,
+        status: HttpStatusCode,
+        request: WebRequest
+    ): ResponseEntity<Any>? {
+        logger.warn(String.format(LOG_MESSAGE_FORMAT, ex.javaClass.simpleName, ex.message), ex)
+
+        val message = ex.fieldErrors
             .stream()
             .map { obj: FieldError -> obj.defaultMessage }
             .collect(Collectors.joining("\n"))
 
         return ResponseEntity.status(HttpStatus.BAD_REQUEST)
             .body(ExceptionResponse(HttpStatus.BAD_REQUEST, METHOD_ARGUMENT_NOT_VALID_EXCEPTION_ERROR_CODE, message))
+
     }
 
-    @ExceptionHandler(HttpMessageNotReadableException::class)
-    fun handleHttpMessageNotReadableException(e: HttpMessageNotReadableException): ResponseEntity<ExceptionResponse> {
+    override fun handleHttpMessageNotReadable(
+        ex: HttpMessageNotReadableException,
+        headers: HttpHeaders,
+        status: HttpStatusCode,
+        request: WebRequest
+    ): ResponseEntity<Any>? {
+        logger.warn(String.format(LOG_MESSAGE_FORMAT, ex.javaClass.simpleName, ex.message), ex)
+
         val response = ExceptionResponse(
             status = HttpStatus.BAD_REQUEST,
             errorCode = "Request-Validation-Fail",
-            message = e.message ?: "Request body is invalid"
+            message = ex.message ?: "Request body is invalid"
         )
 
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(response)
+
     }
 
     @ExceptionHandler(CustomException::class)
-    fun handleCustomException(e: CustomException): ResponseEntity<ExceptionResponse> {
+    fun handleCustomException(ex: CustomException): ResponseEntity<ExceptionResponse> {
+        logger.warn(String.format(LOG_MESSAGE_FORMAT, ex.javaClass.simpleName, ex.message), ex)
+
         val response = ExceptionResponse(
-            status = e.status,
-            errorCode = e.errorCode,
-            message = e.message
+            status = ex.status,
+            errorCode = ex.errorCode,
+            message = ex.message
         )
 
-        return ResponseEntity.status(e.status).body(response)
+        return ResponseEntity.status(ex.status).body(response)
     }
 }
