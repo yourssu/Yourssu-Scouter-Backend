ce6af96ec95248e0c91af7793ae629ad1c8bab45
kwonyj1022 <kwonyj1022@naver.com>
2025-02-15 23:42:48 +0900

    feat: cors 설정 추가

--- src/main/kotlin/com/yourssu/scouter/common/application/support/configuration/WebConfiguration.kt
+++ src/main/kotlin/com/yourssu/scouter/common/application/support/configuration/WebConfiguration.kt
@@ -2,12 +2,16 @@ package com.yourssu.scouter.common.application.support.configuration
 
 import com.yourssu.scouter.common.application.support.authentication.AuthUserInfoArgumentResolver
 import com.yourssu.scouter.common.application.support.authentication.LoginInterceptor
+import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.context.annotation.Configuration
+import org.springframework.http.HttpHeaders
 import org.springframework.web.method.support.HandlerMethodArgumentResolver
+import org.springframework.web.servlet.config.annotation.CorsRegistry
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 
 @Configuration
+@EnableConfigurationProperties(CorsProperties::class)
 class WebConfiguration(
     private val loginInterceptor: LoginInterceptor,
     private val authUserInfoArgumentResolver: AuthUserInfoArgumentResolver
@@ -23,5 +27,16 @@ class WebConfiguration(
 
     override fun addArgumentResolvers(resolvers: MutableList<HandlerMethodArgumentResolver>) {
         resolvers.add(authUserInfoArgumentResolver)
+    }
+
+    override fun addCorsMappings(registry: CorsRegistry) {
+        registry.addMapping("/**")
+            .allowedOrigins(
+                "http://localhost:5173",
+            )
+            .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
+            .allowedHeaders("*")
+            .exposedHeaders(HttpHeaders.LOCATION)
+            .allowCredentials(true)
     }
 }
