648fff029e3aebe25fbe3b5e2b53a5b15986dcda
kwonyj1022 <kwonyj1022@naver.com>
2025-03-07 22:52:15 +0900

    fix: 구글폼 응답 불러올 때 null 값으로 인해 생기는 오류 해결

--- src/main/kotlin/com/yourssu/scouter/common/implement/support/google/GoogleFormsReader.kt
+++ src/main/kotlin/com/yourssu/scouter/common/implement/support/google/GoogleFormsReader.kt
@@ -9,10 +9,10 @@ class GoogleFormsReader(
 
     fun getUserResponses(authorizationHeader: String, formId: String): List<UserResponse> {
         val questionResponse = googleFormsClient.getFormQuestions(authorizationHeader, formId)
-        val questionMap: Map<String, String> = makeQuestionIdAndTitleMap(questionResponse)
+        val questionMap: Map<String, String?> = makeQuestionIdAndTitleMap(questionResponse)
         val formResponses: GoogleFormResponses = googleFormsClient.getFormResponses(authorizationHeader, formId)
 
-        return formResponses.responses?.map { googleUserResponse ->
+        return formResponses.responses.map { googleUserResponse ->
             UserResponse(
                 responseId = googleUserResponse.responseId,
                 createTime = googleUserResponse.createTime,
@@ -20,23 +20,23 @@ class GoogleFormsReader(
                 lastSubmittedTime = googleUserResponse.lastSubmittedTime,
                 responseItems = convertToResponseItems(googleUserResponse, questionMap)
             )
-        } ?: emptyList()
+        }
     }
 
     private fun makeQuestionIdAndTitleMap(questionResponse: GoogleFormQuestions) =
         questionResponse.items
-            ?.mapNotNull { item ->
+            .mapNotNull { item ->
                 val questionId = item.questionItem?.question?.questionId ?: return@mapNotNull null
                 questionId to item.title
-            }?.toMap() ?: emptyMap()
+            }.toMap()
 
     private fun convertToResponseItems(
         googleUserResponse: GoogleUserResponse,
-        questionMap: Map<String, String>
+        questionMap: Map<String, String?>
     ): List<ResponseItem> {
         val responseItems: List<ResponseItem> = googleUserResponse.answers.mapNotNull { (questionId, answer) ->
             val questionTitle = questionMap[questionId] ?: return@mapNotNull null
-            val answerText = answer.textAnswers?.answers?.joinToString(", ") { it.value } ?: ""
+            val answerText = answer.textAnswers?.answers?.joinToString(", ") { it.value.toString() } ?: ""
             ResponseItem(questionTitle, answerText)
         }
         return responseItems
