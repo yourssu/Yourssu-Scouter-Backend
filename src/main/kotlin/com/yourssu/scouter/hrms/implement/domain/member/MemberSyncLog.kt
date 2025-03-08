f6fbae2805f83fe25b47a57c02ce0adea4597b85
kwonyj1022 <kwonyj1022@naver.com>
2025-03-09 07:00:19 +0900

    feat: 합격한 지원자를 멤버로 등록한 기록을 관리하는 domain 추가

---
+++ src/main/kotlin/com/yourssu/scouter/hrms/implement/domain/member/MemberSyncLog.kt
@@ -0,0 +1,22 @@
+package com.yourssu.scouter.hrms.implement.domain.member
+
+import java.time.LocalDateTime
+
+class MemberSyncLog(
+    val id: Long? = null,
+    val syncTime: LocalDateTime,
+) {
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as MemberSyncLog
+
+        return id == other.id
+    }
+
+    override fun hashCode(): Int {
+        return id?.hashCode() ?: 0
+    }
+}
