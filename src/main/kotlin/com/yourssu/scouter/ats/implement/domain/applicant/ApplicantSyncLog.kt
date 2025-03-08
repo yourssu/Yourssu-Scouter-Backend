e5b43a9938efa98d6296260a9fc5acdc198c7693
kwonyj1022 <kwonyj1022@naver.com>
2025-03-09 01:16:25 +0900

    feat: 구글폼에서 지원자 정보를 불러온 기록을 관리하는 domain 추가

---
+++ src/main/kotlin/com/yourssu/scouter/ats/implement/domain/applicant/ApplicantSyncLog.kt
@@ -0,0 +1,25 @@
+package com.yourssu.scouter.ats.implement.domain.applicant
+
+import java.time.LocalDateTime
+
+class ApplicantSyncLog(
+    val id: Long? = null,
+    val applicantSemesterId: Long,
+    val formId: String,
+    val responseId: String,
+    val lastSyncTime: LocalDateTime,
+) {
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as ApplicantSyncLog
+
+        return id == other.id
+    }
+
+    override fun hashCode(): Int {
+        return id.hashCode()
+    }
+}
