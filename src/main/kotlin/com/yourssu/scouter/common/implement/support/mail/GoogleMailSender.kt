4e7d3509153b6e5c404c07e9a4de60b669362543
kwonyj1022 <kwonyj1022@naver.com>
2025-04-20 20:08:31 +0900

    feat: 구글 api를 통해 이메일을 전송하는 메일 전송 구현체 추가

---
+++ src/main/kotlin/com/yourssu/scouter/common/implement/support/mail/GoogleMailSender.kt
@@ -0,0 +1,44 @@
+package com.yourssu.scouter.common.implement.support.mail
+
+import com.yourssu.scouter.common.business.domain.mail.MailSender
+import com.yourssu.scouter.common.business.domain.mail.MailData
+import com.yourssu.scouter.common.implement.domain.mail.MimeMessageBuilder
+import com.yourssu.scouter.common.implement.domain.mail.MimeMessageBuilderResolver
+import com.yourssu.scouter.common.implement.support.google.GoogleMailClient
+import jakarta.mail.Session
+import jakarta.mail.internet.MimeMessage
+import java.io.ByteArrayOutputStream
+import java.util.*
+import org.springframework.stereotype.Component
+
+@Component
+class GoogleMailSender(
+    private val googleMailClient: GoogleMailClient,
+    private val messageBuilderResolver: MimeMessageBuilderResolver,
+) : MailSender {
+
+    override fun send(mailData: MailData, accessToken: String) {
+        val messageBuilder: MimeMessageBuilder = messageBuilderResolver.resolve(mailData)
+
+        val properties = Properties().apply {
+            put("mail.smtp.auth", "true")
+            put("mail.smtp.starttls.enable", "true")
+            put("mail.smtp.host", "smtp.gmail.com")
+            put("mail.smtp.port", "587")
+        }
+        val session = Session.getInstance(properties, null)
+        val message: MimeMessage = messageBuilder.build(mailData, session)
+        val encodedEmail: String = encodeMessage(message)
+
+        googleMailClient.sendEmail(encodedEmail, accessToken)
+    }
+
+    private fun encodeMessage(message: MimeMessage): String {
+        val outputStream = ByteArrayOutputStream()
+        message.writeTo(outputStream)
+
+        val encoder: Base64.Encoder = Base64.getUrlEncoder()
+
+        return encoder.encodeToString(outputStream.toByteArray())
+    }
+}
