7ac80a287cef6e15e0388c0a5c0adc13e72dd2aa
kwonyj1022 <kwonyj1022@naver.com>
2025-07-19 14:23:27 +0900

    feat: 메일 예약 api 추가

---
+++ src/main/kotlin/com/yourssu/scouter/common/application/domain/mail/MailReservationController.kt
@@ -0,0 +1,37 @@
+package com.yourssu.scouter.common.application.domain.mail
+
+import com.yourssu.scouter.common.application.support.authentication.AuthUser
+import com.yourssu.scouter.common.application.support.authentication.AuthUserInfo
+import com.yourssu.scouter.common.business.domain.mail.MailService
+import com.yourssu.scouter.common.implement.domain.mail.MailReserveCommand
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestPart
+import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.multipart.MultipartFile
+
+@RestController
+@RequestMapping("/api/mails/reservation")
+class MailReservationController(
+    private val mailService: MailService
+) {
+
+    @PostMapping
+    fun reserveMail(
+        @AuthUser authUserInfo: AuthUserInfo,
+        @RequestPart request: MailReserveRequest,
+        @RequestPart(required = false) inlineImages: List<MultipartFile>?,
+        @RequestPart(required = false) attachments: List<MultipartFile>?
+    ): ResponseEntity<Unit> {
+        val command: MailReserveCommand = request.toCommand(
+            userId = authUserInfo.userId,
+            request = request,
+            inlineImages = inlineImages,
+            attachments = attachments
+        )
+        mailService.reserveMail(command)
+
+        return ResponseEntity.ok().build()
+    }
+} 
---
+++ src/main/kotlin/com/yourssu/scouter/common/application/domain/mail/MailReserveRequest.kt
@@ -0,0 +1,46 @@
+package com.yourssu.scouter.common.application.domain.mail
+
+import com.yourssu.scouter.common.business.domain.mail.MailBodyFormat
+import com.yourssu.scouter.common.implement.domain.mail.MailReserveCommand
+import java.time.LocalDateTime
+import org.springframework.format.annotation.DateTimeFormat
+import org.springframework.web.multipart.MultipartFile
+
+data class MailReserveRequest(
+
+    val receiverEmailAddresses: List<String>,
+
+    val ccEmailAddresses: List<String>?,
+
+    val bccEmailAddresses: List<String>?,
+
+    val mailSubject: String,
+
+    val mailBody: String,
+
+    val bodyFormat: String,
+
+    @field:DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME)
+    val reservationTime: LocalDateTime
+) {
+
+    fun toCommand(
+        userId: Long,
+        request: MailReserveRequest,
+        inlineImages: List<MultipartFile>?,
+        attachments: List<MultipartFile>?
+    ): MailReserveCommand {
+        return MailReserveCommand(
+            senderUserId = userId,
+            receiverEmailAddresses = receiverEmailAddresses,
+            ccEmailAddresses = ccEmailAddresses ?: emptyList(),
+            bccEmailAddresses = bccEmailAddresses ?: emptyList(),
+            mailSubject = mailSubject,
+            mailBody = mailBody,
+            bodyFormat = MailBodyFormat.valueOf(bodyFormat),
+            inlineImages = inlineImages ?: emptyList(),
+            attachments = attachments ?: emptyList(),
+            reservationTime = request.reservationTime
+        )
+    }
+}
