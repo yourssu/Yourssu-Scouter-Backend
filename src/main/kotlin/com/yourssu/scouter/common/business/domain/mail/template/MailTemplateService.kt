4bb02b400c2f409585836b3237ee652e4a88b76a
kim nan seul <154604350+seulnan@users.noreply.github.com>
2025-09-03 17:19:35 +0900

    [#111]; feat(mail): 메일 템플릿목록 조회기능

--- src/main/kotlin/com/yourssu/scouter/common/business/domain/mail/template/MailTemplateService.kt
+++ src/main/kotlin/com/yourssu/scouter/common/business/domain/mail/template/MailTemplateService.kt
@@ -4,6 +4,8 @@ import com.yourssu.scouter.common.implement.domain.mail.template.MailTemplate
 import com.yourssu.scouter.common.implement.domain.mail.template.MailTemplateRepository
 import com.yourssu.scouter.common.implement.domain.mail.template.TemplateVariable
 import org.springframework.stereotype.Service
+import org.springframework.data.domain.Page
+import org.springframework.data.domain.Pageable
 
 @Service
 class MailTemplateService(
@@ -11,5 +13,9 @@ class MailTemplateService(
 ) {
     fun createTemplate(template: MailTemplate): MailTemplate {
         return mailTemplateRepository.save(template)
+    }
+
+    fun readTemplates(pageable: Pageable): Page<MailTemplate> {
+        return mailTemplateRepository.findAll(pageable)
     }
 }
