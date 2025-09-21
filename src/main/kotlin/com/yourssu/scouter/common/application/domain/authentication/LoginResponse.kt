197165bfe3d4c57dece6e0fa6d49a0e2b3ce55f5
kim nan seul <154604350+seulnan@users.noreply.github.com>
2025-09-22 02:18:09 +0900

    [#129]; feat(auth): 관련 dto에 tokenType 필드 추가
    LoginResponse/TokenRefreshResponse에 tokenType 필드 추가

--- src/main/kotlin/com/yourssu/scouter/common/application/domain/authentication/LoginResponse.kt
+++ src/main/kotlin/com/yourssu/scouter/common/application/domain/authentication/LoginResponse.kt
@@ -3,11 +3,13 @@ package com.yourssu.scouter.common.application.domain.authentication
 import com.yourssu.scouter.common.business.domain.authentication.LoginResult
 
 data class LoginResponse(
+    val tokenType: String,
     val accessToken: String,
     val refreshToken: String,
 ) {
     companion object {
         fun from(loginResult: LoginResult): LoginResponse = LoginResponse(
+            tokenType = "Bearer",
             accessToken = loginResult.accessToken,
             refreshToken = loginResult.refreshToken
         )
