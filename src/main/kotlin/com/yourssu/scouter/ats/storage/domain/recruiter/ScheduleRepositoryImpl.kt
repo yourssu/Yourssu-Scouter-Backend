10326408c16b224ed85fec4ed7324717ae969602
leeminseo <leeminseo0923@gmail.com>
2025-09-25 18:43:06 +0900

    [#140]; feat: unique key 중복 예외처리 추가

--- src/main/kotlin/com/yourssu/scouter/ats/storage/domain/recruiter/ScheduleRepositoryImpl.kt
+++ src/main/kotlin/com/yourssu/scouter/ats/storage/domain/recruiter/ScheduleRepositoryImpl.kt
@@ -2,6 +2,8 @@ package com.yourssu.scouter.ats.storage.domain.recruiter
 
 import com.yourssu.scouter.ats.implement.domain.recruiter.InterviewSchedule
 import com.yourssu.scouter.ats.implement.domain.recruiter.ScheduleRepository
+import com.yourssu.scouter.ats.storage.domain.support.exception.DuplicateScheduleException
+import org.springframework.dao.DataIntegrityViolationException
 import org.springframework.stereotype.Repository
 
 @Repository
@@ -11,7 +13,13 @@ class ScheduleRepositoryImpl(
 
     override fun saveAll(schedules: List<InterviewSchedule>) {
         val entities = ScheduleEntity.fromDomainList(schedules)
-        jpaScheduleRepository.saveAll(entities)
+        try {
+            jpaScheduleRepository.saveAll(entities)
+        } catch (e: DataIntegrityViolationException) {
+            if (e.message?.contains("unique_interview_schedule") == true)
+                throw DuplicateScheduleException("이미 해당 시간에 면접이 예정되어 있습니다.")
+            throw e
+        }
     }
 
     override fun findAllByPartId(partId: Long) : List<InterviewSchedule> {
