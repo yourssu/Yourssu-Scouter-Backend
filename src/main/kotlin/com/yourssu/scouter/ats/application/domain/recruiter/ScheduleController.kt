76618edbf61d6f19352d95e4fb53ffa197ab3ac1
leeminseo <leeminseo0923@gmail.com>
2025-10-16 15:01:28 +0900

    [#145]; fix: "스케쥴" → "스케줄" 오타 수정 in ScheduleController

--- src/main/kotlin/com/yourssu/scouter/ats/application/domain/recruiter/ScheduleController.kt
+++ src/main/kotlin/com/yourssu/scouter/ats/application/domain/recruiter/ScheduleController.kt
@@ -15,7 +15,7 @@ class ScheduleController(
     private val scheduleService: ScheduleService
 ) {
 
-    @Operation(summary = "면접 스케쥴 추가 API", description = "면접 일정을 추가하는 API 입니다.")
+    @Operation(summary = "면접 스케줄 추가 API", description = "면접 스케줄을 추가하는 API 입니다.")
     @ApiResponse(description = "CREATED", responseCode = "201")
     @PostMapping("/schedule")
     fun createSchedules(@RequestBody schedules: List<CreateScheduleRequest>): ResponseEntity<Unit> {
@@ -26,8 +26,8 @@ class ScheduleController(
     }
 
     @Operation(
-        summary = "면접 스케쥴 조회 API",
-        description = "면접 일정 조회 API 입니다. 추후 partID를 입력하지 않으면 전체 조회가 되도록 변경 예정"
+        summary = "면접 스케줄 조회 API",
+        description = "면접 스케줄 조회 API 입니다. 추후 partID를 입력하지 않으면 전체 조회가 되도록 변경 예정"
     )
     @GetMapping("/schedule")
     fun getSchedules(@RequestParam partId: Long) = ResponseEntity.ok(
