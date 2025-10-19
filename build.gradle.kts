122fbef7779eb6f4531f62c24131fd27a8315674
leeminseo <leeminseo0923@gmail.com>
2025-10-19 20:08:41 +0900

    [#150]; chore: Mock 테스트 의존성 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -52,7 +52,8 @@ dependencies {
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
 	testImplementation("org.assertj:assertj-core")
-	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+    testImplementation("org.mockito.kotlin:mockito-kotlin:5.2.1")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 
     // swagger
     implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.8.13")
