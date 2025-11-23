87e8da43ac33c9cb4094f633a7903022f36c4e2c
leeminseo <leeminseo0923@gmail.com>
2025-11-23 13:04:33 +0900

    [#183]; refactor: 전체 날짜 형식 yyyy-MM-dd로 통일

--- src/main/kotlin/com/yourssu/scouter/hrms/application/domain/member/LastMemberSyncTimeResponse.kt
+++ src/main/kotlin/com/yourssu/scouter/hrms/application/domain/member/LastMemberSyncTimeResponse.kt
@@ -1,10 +1,8 @@
 package com.yourssu.scouter.hrms.application.domain.member
 
-import com.fasterxml.jackson.annotation.JsonFormat
 import java.time.LocalDateTime
 
 data class LastMemberSyncTimeResponse(
 
-    @JsonFormat(pattern = "yyyy.MM.dd HH:mm")
     val lastUpdatedTime: LocalDateTime?,
 )
