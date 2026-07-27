05502ab2d4b114663b14be03bfc71df75e4159a6
eeeeeaaan <wjdwjddla73@naver.com>
2026-07-27 11:20:16 +0900

    [#319]; fix: 마이그레이션 파일 수정

--- src/main/resources/db/migration/V28__create_interview_evaluation_tables.sql
+++ src/main/resources/db/migration/V28__create_interview_evaluation_tables.sql
@@ -29,10 +29,25 @@ CREATE TABLE interview_evaluation (
                                       interview_evaluation_item_id BIGINT  NOT NULL,
                                       member_id                BIGINT      NOT NULL,
                                       score                    INT         NOT NULL,
-                                      result                   VARCHAR(30) NOT NULL DEFAULT 'PENDING', -- PENDING, FINAL_PASS, INTERVIEW_FAIL
-                                      overall_comment          TEXT        NULL,
-                                      submitted_at             DATETIME(6)  NULL,
 
                                       CONSTRAINT fk_interview_evaluation_applicant FOREIGN KEY (applicant_id) REFERENCES applicant (id),
                                       CONSTRAINT fk_interview_evaluation_item FOREIGN KEY (interview_evaluation_item_id) REFERENCES interview_evaluation_item (id)
 );
+
+-- 4. Final Evaluation (최종 평가 결과)
+CREATE TABLE final_evaluation (
+                                  id                  BIGINT AUTO_INCREMENT PRIMARY KEY,
+                                  member_id           BIGINT      NOT NULL,
+                                  interview_rubric_id BIGINT      NOT NULL,
+                                  applicant_id        BIGINT      NOT NULL,
+                                  overall_comment     TEXT        NULL,
+                                  interview_result    VARCHAR(30) NOT NULL DEFAULT 'PENDING',
+                                  score               INT         NOT NULL,
+                                  submit              BOOLEAN     NOT NULL DEFAULT FALSE,
+                                  submitted_at        DATETIME(6) NULL,
+
+                                  CONSTRAINT fk_final_evaluation_member FOREIGN KEY (member_id) REFERENCES member (id),
+                                  CONSTRAINT fk_final_evaluation_rubric FOREIGN KEY (interview_rubric_id) REFERENCES interview_rubric (id),
+                                  CONSTRAINT fk_final_evaluation_applicant FOREIGN KEY (applicant_id) REFERENCES applicant (id)
+);
+
