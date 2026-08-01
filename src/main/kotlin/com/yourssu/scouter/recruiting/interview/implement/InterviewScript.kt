811dc661bfe3de8b222b45aa33eabdfc1a47bd2a
leeminseo <leeminseo0923@gmail.com>
2026-08-01 17:13:31 +0900

    [#355]; refactor: `InterviewScript` Legacy 제거

--- src/main/kotlin/com/yourssu/scouter/recruiting/interview/implement/InterviewScript.kt
+++
@@ -1,7 +0,0 @@
-package com.yourssu.scouter.recruiting.interview.implement
-
-class InterviewScript(
-    val partId: Long,
-    val opening: String,
-    val closing: String,
-)
--- src/main/kotlin/com/yourssu/scouter/recruiting/interview/storage/JpaInterviewScriptRepository.kt
+++
@@ -1,5 +0,0 @@
-package com.yourssu.scouter.recruiting.interview.storage
-
-import org.springframework.data.jpa.repository.JpaRepository
-
-interface JpaInterviewScriptRepository : JpaRepository<InterviewScriptEntity, Long>
