4452fcb45ccb5703be37f0f4e05edfa138075971
yueunfive <122276414+yueunfive@users.noreply.github.com>
2026-08-07 13:50:03 +0900

    [#364]; refactor: 모집 학기 응답을 year/term으로 구조화
    - ReadSemesterResponse의 semester 문자열 필드("26-2학기")를 year, term int 필드로 분리

--- src/main/kotlin/com/yourssu/scouter/common/semester/application/dto/ReadSemesterResponse.kt
+++ src/main/kotlin/com/yourssu/scouter/common/semester/application/dto/ReadSemesterResponse.kt
@@ -1,18 +1,19 @@
 package com.yourssu.scouter.common.semester.application.dto
 
 import com.yourssu.scouter.common.semester.business.dto.SemesterDto
-import com.yourssu.scouter.common.support.business.utils.SemesterConverter
 
 data class ReadSemesterResponse(
     val semesterId: Long,
-    val semester: String,
+    val year: Int,
+    val term: Int,
 ) {
 
     companion object {
         fun from(semesterDto: SemesterDto): ReadSemesterResponse {
             return ReadSemesterResponse(
                 semesterId = semesterDto.id,
-                semester = SemesterConverter.convertToStringWithTermLabel(semesterDto)
+                year = semesterDto.year.value,
+                term = semesterDto.term.intValue,
             )
         }
     }
