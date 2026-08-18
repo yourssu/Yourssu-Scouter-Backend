64bbce82b94dd364ca9e7e6752c45896eeeabde2
eeeeeaaan <wjdwjddla73@naver.com>
2026-08-18 09:40:22 +0900

    fix : 평가 제출 시 면접 질문 수정 불가능(요구조건 매핑 포함)

--- src/main/kotlin/com/yourssu/scouter/recruiting/interview/business/InterviewRequirementService.kt
+++ src/main/kotlin/com/yourssu/scouter/recruiting/interview/business/InterviewRequirementService.kt
@@ -60,7 +60,7 @@ class InterviewRequirementService(
         if (existingRubric != null && existingRubric.items.isNotEmpty()) {
             val itemIds = existingRubric.items.mapNotNull { it.id }
             if (interviewEvaluationReader.existsByInterviewEvaluationItemIdIn(itemIds)) {
-                throw RubricLockedException("해당 파트에 면접 평가(임시저장 포함)가 존재해 수정 불가")
+                throw RubricLockedException("해당 파트에 면접 평가가 존재해 수정 불가")
             }
         }
 
