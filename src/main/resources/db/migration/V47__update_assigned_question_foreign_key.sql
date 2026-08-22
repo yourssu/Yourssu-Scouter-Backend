485dc25d0df55a63db0fe3c2b97154f924186512
eeeeeaaan <wjdwjddla73@naver.com>
2026-08-22 20:41:58 +0900

    fix : 마이그레이션 파일 수정

--- src/main/resources/db/migration/V47__update_assigned_question_foreign_key.sql
+++ src/main/resources/db/migration/V47__update_assigned_question_foreign_key.sql
@@ -1,8 +1,3 @@
--- V47__update_assigned_question_foreign_key.sql
-
-ALTER TABLE assigned_question
-    DROP FOREIGN KEY fk_assigned_question_assigned_interviewer;
-
 ALTER TABLE assigned_question
     ADD CONSTRAINT fk_assigned_question_assigned_member
         FOREIGN KEY (assigned_member_id) REFERENCES member (id);
